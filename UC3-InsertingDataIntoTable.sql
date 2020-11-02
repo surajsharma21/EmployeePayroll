f414badbf2e5090d89b342e8039e5d8746d255b5
Suraj <surajshrm000@gmail.com>
2020-11-02 17:07:03 +0530

    [Suraj] Add: Inserting Data rows into table

---
+++ UC3-InsertingDataIntoTable.sql
@@ -0,0 +1,17 @@
+INSERT INTO [Employee_Payroll]
+(
+[Name], [Salary], [Start_Date]
+)
+VALUES
+(
+    'Swapnil',50000.00,'09-18-2020'
+),
+(
+    'Billy', 40000.00, '04-28-2018'
+),
+(
+    'Terisa',60000.00,'11-10-2018'
+),
+(
+    'Bob',55000.00,'01-01-2017'
+);
